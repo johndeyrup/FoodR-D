53cb4e12ab6d7b6237acfba45e7ea3ef1e550679
John <john.deyrup@gmail.com>
2015-05-13 19:43:38 +0900

    Website

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("nav01").innerHTML =
+    "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='research.html'>R & D</a></li>" +
+    "</ul>";
